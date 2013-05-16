ae2873d4bf6854b7fb0565a3305d9f71e20ccae5
amoa400 <amoa400@gmail.com>
2013-05-16 16:20:35 +0800

    reset province

--- Tour/Lib/Action/LineAction.class.php
+++ Tour/Lib/Action/LineAction.class.php
@@ -3,8 +3,8 @@
 class LineAction extends Action {
 
 	private $pointTypeList;
-	public $zhoubian = array(3, 4, 6);
-	public $guonei = array(1);
+	public $zhoubian = array(1,2,3,4,6);
+	public $guonei = array(5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32);
 
 	// 线路显示页面
     public function show() {
